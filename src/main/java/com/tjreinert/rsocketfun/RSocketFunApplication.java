16d360ff3960df35b0e467c37b71d6b94490c366
Jinkoo <swimy2012@gmail.com>
2020-03-05 18:49:38 -0600

    Fix spelling error in main class

--- src/main/java/com/tjreinert/rsocketfun/RSocketFunApplication.java
+++ src/main/java/com/tjreinert/rsocketfun/RSocketFunApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RsocketFunApplication {
+public class RSocketFunApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RsocketFunApplication.class, args);
+		SpringApplication.run(RSocketFunApplication.class, args);
 	}
 
 }
